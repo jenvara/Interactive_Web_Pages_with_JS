b1606d8971ed4ec9800b5d2fcc04321730b823e9
Jennifer Varahala <Jennifer Varahala>
2016-10-02 11:34:15 -0400

    Added variables & functions to JS code

--- project_start/js/app.js
+++ project_start/js/app.js
@@ -1,7 +1,13 @@
 //Problem: User interaction doesn't provide desired results.
 //Solution: Add interativity so the user can manage daily tasks.
 
-//Add a new task
+var taskInput = document.getElementById("new-task"); //new-task
+var addButton = document.getElementsByTagName("button")[0]; //first button
+var incompleteTasksHolder = document.getElementById("incomplete-tasks"); //incomplete-tasks
+var compeltedTasksHolder = document.getElementById("completed-tasks"); //completed-tasks
+
+//Add a new task 
+var addTask = function() {
 	//When the button is pressed
 	//Create a new list item with the text from #new-task:
 		//input (checkbox)
@@ -10,8 +16,10 @@
 		//button.edit
 		//button.delete
 		//Each element, needs modified and appended
+}
 
 //Edit an existing task
+var editTask = function() {
 	//When the Edit button is pressed	
 		//if the class of the parent is .editMode
 			//Switch from .editMode
@@ -21,14 +29,19 @@
 			//input value becomes the label's text
 			
 		//Toggle .editMode on the parent
+}
 		
 //Delete an existing task 
+var deleteTask = function() {
 	//When the Delete button is pressed	
 		//Remove the parent list item from the ul
+}
 
 //Mark a task as complete
+var taskCompleted = function() {
 	//When the Checkbox is checked	
 		//Append the task list item to the #completed-tasks	
+}
 
 //Mark a task as incomplete
 	//When the Checkbox is unchecked
